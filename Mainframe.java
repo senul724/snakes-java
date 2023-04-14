fe4c8226a39e0cd27fcd6cd66968701b85fbdd8d
Senul <hardlove.mrplus@gmail.com>
2023-04-14 12:07:46 +0530

    minor fixes

--- Mainframe.java
+++ Mainframe.java
@@ -1,17 +1,18 @@
 import javax.swing.JFrame;
 
 public class Mainframe extends JFrame{
+    String title = "This is a test drawing with swing timer";
+    Label label = new Label(200, 0, 800, 100, title);
+    static Label scoreBoard = new Label(0, 0, 200, 100, "score 0");
+    MainPanel panel = new MainPanel(0, 100, 1000, 900);
 
     Mainframe(){
-        String title = "This is a test drawing with swing timer";
-        Label label = new Label(0, 0, 1000, 100, title);
-        MainPanel panel = new MainPanel(0, 100, 1000, 900);
-
         this.setTitle("test for panel");
         this.setSize(1000, 1000);
         this.setLayout(null);
         this.add(label);
         this.add(panel);
+        this.add(scoreBoard);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
